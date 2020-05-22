d7805f932d55129cc10c277ad345daf49c46ea40
Jon Corbett <corbett.jo@husky.neu.edu>
2020-05-21 20:40:39 -0700

    removed image download

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -183,28 +183,6 @@ export const query = graphql`
               title
             }
           }
-          mainImage {
-            crop {
-              _key
-              _type
-              top
-              bottom
-              left
-              right
-            }
-            hotspot {
-              _key
-              _type
-              x
-              y
-              height
-              width
-            }
-            asset {
-              _id
-            }
-            alt
-          }
           slug {
             current
           }
